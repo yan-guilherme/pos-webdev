075136d64028c7d6044851270a627aca5d685526
Frank Coutinho <frankcoutinho@outlook.com>
2019-11-09 09:35:38 -0300

    Criado beer model

--- angular-crud/src/app/core/services/beer.service.ts
+++ angular-crud/src/app/core/services/beer.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap, map } from 'rxjs/operators';
-// import { Beer } from 'src/core/model/beer';
+import { Beer } from 'src/core/model/beer.model';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
---
+++ angular-crud/src/core/model/beer.model.ts
@@ -0,0 +1,1 @@
+export class Beer { _id: string; name: string; description: string; }
