460f442db9fb1fd27ab4c4f8a5b0b91d8c80f177
otavioalencar <otavioalencar59@gmail.com>
2019-11-09 16:28:49 -0300

    Otavio de Alencar Pereira / Alessandro Ribeiro Frias

--- angular-crud/src/app/pages/beer/beer.component.ts
+++ angular-crud/src/app/pages/beer/beer.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-// import { BeerService } from 'src/app/core/services/beer.service';
+import { ApiService } from 'src/app/core/services/api.service';
+import { Router } from '@angular/router';
 import { Beer } from 'src/app/core/model/beer.model';
+import { Observable } from 'rxjs';
+
+/** Componente default que busca e lista todas as cervejas cadastradas no server json*/
 
 @Component({
   selector: 'app-beer',
@@ -10,16 +14,24 @@ import { Beer } from 'src/app/core/model/beer.model';
 export class BeerComponent implements OnInit {
 
   public beers;
+  //beers: Observable<Beer[]>;
 
-  constructor(    
-    // private beerService: BeerService
-  ) { 
-    
-  }
+  constructor(private api: ApiService,private router: Router) { }
 
+  // metodo utilizado para carregar as cervejas assim que a página é aberta
   ngOnInit() {
-          
+    this.api.getBeers()
+      .subscribe(res => {
+        this.beers = res;
+        //console.log(this.beers);
+        // this.isLoadingResults = false;
+      }, err => {
+        console.log(err);
+        // this.isLoadingResults = false;
+      });
   }
-
-        
+  
+  editBeer(beer){
+    this.router.navigate(['/beer-edit', beer]);
+  }
 }
